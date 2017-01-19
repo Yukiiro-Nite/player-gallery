63a537a83d384e576fe1ce404c4e9e840edb02d9
Yukiiro-Nite <e.kazuma@gmail.com>
2017-01-18 21:45:15 -0500

    Michael.B | Added a server file that starts an express app and registers commands to the app.

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3008;
 
+const log = require('./utils/logger.js').logUtil('Server');
+const registerCommands = require('./utils/registerCommands');
+
+app.use(express.static('public'));
+
+
+registerCommands(app);
+
+app.listen(port, ()=>{
+  log(`Listening on port: ${port}`);
+});
