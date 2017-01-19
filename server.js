9d77efb6fa06feb08100ecba112c9c1802d4e2be
Yukiiro-Nite <e.kazuma@gmail.com>
2017-01-18 23:05:33 -0500

    Michael.B | Added body-parser to the server to allow use of body json.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 const port = 3008;
 
 const log = require('./utils/logger.js').logUtil('Server');
 const registerCommands = require('./utils/registerCommands');
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 
